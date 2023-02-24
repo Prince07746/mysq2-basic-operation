4a9e23987daf940446aa9f0a97320f39f44d86f7
Prince07746 <98363125+Prince07746@users.noreply.github.com>
2023-02-25 01:42:43 +0300

    comment changes

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,22 @@
 function start(){
     var name = document.getElementById('password');
     if(name.value == 2022){
-        document.getElementById('demo').innerHTML = "welcome";
+        document.getElementById('demo').innerHTML = "welcome mr juuko it's always a pleasure to see you";
         document.getElementById('form').style.display = "flex";
         document.getElementById('pass').style.display = "none";
-prompt('are you xv');
-    }else{
+    }
+    else if(name.value == 2020){
+        document.getElementById('demo').innerHTML = "welcome mr samuel keep learning tomorrow will be better";
+        document.getElementById('form').style.display = "flex";
+        document.getElementById('pass').style.display = "none";
+    }
+    else if(name.value == 2023){
+        document.getElementById('demo').innerHTML = "welcome mr prince can you , don't worry i'm fine and on my best performance don't over stress about me";
+        document.getElementById('form').style.display = "flex";
+        document.getElementById('pass').style.display = "none";
+    }
+    else{
         document.getElementById('demo').innerHTML = "wrong password";
     }
+
 }
