0f9440095609734bab7ec9c4ec76fa83ccbd5cf8
Prince07746 <98363125+Prince07746@users.noreply.github.com>
2023-02-26 13:41:04 +0300

    add mysql2

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-
+const mysql = require('mysql2');
 
 var app = express();
 var port = process.env.PORT || 3030;
 
 app.use(express.static(__dirname+'/pages'));
+
+
+// DATA BASE CONNECTION
+const connection = mysql.createConnection({
+  host: 'us-cdbr-east-06.cleardb.net',
+  user: 'b3b86034d97059',
+  password: '5157a6c3',
+  database: 'heroku_2073bff5db0dcc9'
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log('Connected!');
+});
+
 
 
 app.get('/',function(req,res){
@@ -20,10 +35,13 @@ app.post('/formula',function(req,res){
 
 
 app.get('/tables',function(req,res){
-  res.send('this is the database');
-  console.log('dtb finded on server point with tables');
+
+connection.query('SHOW TABLES', function (err, results, fields) {
+  if (err) throw err;
+  res.send(results);
 });
-  
+
+});    
     
     
 app.listen(port,function(err){
