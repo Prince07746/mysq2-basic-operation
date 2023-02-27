f1912743e0581525dd41bf2b2cbd61456af90ff4
Prince07746 <98363125+Prince07746@users.noreply.github.com>
2023-02-28 02:00:13 +0300

    mysql2 to mysql

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const mysql = require('mysql2');
+const mysql = require('mysql');
 
 var app = express();
 var port = process.env.PORT || 3030;
