3e3b8959c67b7d5f0fab0a735bb587800183eb21
Prince07746 <98363125+Prince07746@users.noreply.github.com>
2023-02-28 10:20:49 +0300

    changing connection data base after connection

--- app.js
+++ app.js
@@ -39,17 +39,17 @@ connection.query('SELECT * FROM user', function (err, results, fields) {
   if (err) throw err;
   res.send(results);
 });
-
-});
-
-
 connection.end((err) => {
   if (err) {
     console.error('Error closing the database connection: ' + err.stack);
     return;
   }
   console.log('Database connection closed.');
+});  
+
 });
+
+
     
     
 app.listen(port,function(err){
